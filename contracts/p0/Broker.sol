5a4674226ba1de302879d2ee89710e0e1be9be03
Larry the Cucumber <larrythecucumber321@gmail.com>
2023-06-06 14:45:10 -0700

    test: small change

--- contracts/p0/Broker.sol
+++ contracts/p0/Broker.sol
@@ -69,7 +69,7 @@ contract BrokerP0 is ComponentP0, IBroker {
         // Apply Gnosis EasyAuction-specific resizing
         req = resizeTrade(req, GNOSIS_MAX_TOKENS);
 
-        req.sell.erc20().safeTransferFrom(caller, address(trade), req.sellAmount);
+        req.sell.erc20().transferFrom(caller, address(trade), req.sellAmount);
 
         trade.init(this, caller, gnosis, auctionLength, req);
         return trade;
